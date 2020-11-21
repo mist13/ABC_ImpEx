6fcaa55e3d3180b3ef4b08deea77b934384d6ccc
mist13 <74824528+mist13@users.noreply.github.com>
2020-11-21 16:31:45 +0100

    Add files via upload

---
+++ translations/locale_de.ts
@@ -0,0 +1,177 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>QPlatformTheme</name>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="251"/>
+        <source>Open</source>
+        <translation>Öffnen</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="264"/>
+        <source>Import</source>
+        <translation>Importieren</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="311"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="324"/>
+        <source>Export</source>
+        <translation>Exportieren</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="366"/>
+        <source>Save</source>
+        <translation>Speichern</translation>
+    </message>
+</context>
+<context>
+    <name>abc_ImpEx</name>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="33"/>
+        <source>This plugin imports ABC notation from a file or the clipboard and exports the current score to ABC.
+Executables of abc2xml and/or xml2abc are required.</source>
+        <translation>Dieses Plugin importiert ABC-Notation aus einer Datei oder der Zwischenablage und exportiert die aktiven Partitur als ABC.
+Die Programme abc2xml bzw. xml2abc werden benötigt.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="49"/>
+        <source>
+ INSTALLATION
+ ==========
+
+        Copy abc2xml.py and/or xml2abc.py into this plugin&apos;s folder:
+        %1
+
+        Should you already have the files in a different location,
+        enter their full path into the ini file in the same folder.
+
+
+        If you are running Windows, make sure that Python is installed
+        and that the path to Python is in your PATH environment variable.
+
+        Alternatively, use abc2xml.exe and xm2abc.exe files instead.</source>
+        <translation>
+ INSTALLATION
+ ==========
+
+        abc2xml.py bzw xml2abc.py in diesen Pluginordner kopieren:
+        %1
+
+        Sollten die Dateien schon woanders installiert sein,
+        diesen Pfad in die Ini-Datei im gleichen Ordner eintragen.
+
+        Unter Windows muss Python installiert sein und der Pfad
+        zu Python in der Umgebungsvariable PATH inkludiert sein.
+
+        Alternativ kann man auch abc2xml.exe und xm2abc.exe verwenden.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="105"/>
+        <location filename="../abc_ImpEx.qml" line="303"/>
+        <location filename="../abc_ImpEx.qml" line="352"/>
+        <source>Python not found!.
+Please check your installation and
+environment variables.</source>
+        <translation>Python wurde nicht gefunden.
+Überprüfen Sie Ihre Installation und
+die Umgebungsvariablen.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="148"/>
+        <source>Please choose a file</source>
+        <translation>Bitte Datei auswählen</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="162"/>
+        <source>Please select a destination</source>
+        <translation>Bitte Zieldatei wählen</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="197"/>
+        <source>This plugin needs abc2xml and/or xml2abc from &lt;a href=&apos;https://wim.vree.org/svgParse&apos;&gt;wim.vree.org&lt;/a&gt; to work.</source>
+        <translation>Dieses Plugin benötigt abc2xml bzw. xml2abc von &lt;a href=&apos;https://wim.vree.org/svgParse&apos;&gt;wim.vree.org&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="240"/>
+        <source>score line-break = $</source>
+        <translation>neue Notenzeile = $</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="295"/>
+        <source>No abc to import.</source>
+        <translation>Kein abc zum Importieren vorhanden.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="301"/>
+        <source>This function needs abc2xml.
+Copy file into abc_ImpEx folder
+or enter path into abc_ImpEx.ini.</source>
+        <translation>Diese Funktion benötigt abc2xml.
+Datei in den abc_ImpEx-Ordner kopieren
+oder den Pfad in die abc_ImpEx.ini eintragen.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="350"/>
+        <source>This function needs xml2abc.
+Copy file into abc_ImpEx folder
+or enter path into abc_ImpEx.ini.</source>
+        <translation>Diese Funktion benötigt xml2abc.
+Datei in den abc_ImpEx-Ordner kopieren
+oder den Pfad in die abc_ImpEx.ini eintragen.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="357"/>
+        <source>No score to export.</source>
+        <translation>Keine Noten zum Exportieren vorhanden.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="378"/>
+        <source>No abc to save.</source>
+        <translation>Kein abc zum Speichern vorhanden.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="58"/>
+        <source>
+ IMPORT
+ ======
+
+            1. Paste ABC or select file using button &apos;Open&apos;.
+
+            2. Change ABC as desired and/or select part of it.
+
+            3. Import into score using button &apos;Import&apos;.
+            
+ EXPORT
+ ======
+
+            1. To translate active score, click button &apos;Export&apos;.
+
+            2. Change ABC as desired and/or select part of it.
+
+            3. Save into file using button &apos;Save&apos;.</source>
+        <translation>
+ IMPORT
+ ======
+
+            1. ABC einfügen oder eine Datei über die Taste &apos;Öffnen&apos; wählen.
+
+            2. ABC nach Wunsch bearbeiten bzw. Text markieren.
+
+            3. Als Partitur importieren mittels &apos;Import&apos;.
+            
+ EXPORT
+ ======
+
+            1. Zum Übersetzen der aktiven Partitur &apos;Export&apos; drücken.
+
+            2. ABC nach Wunsch bearbeiten bzw. Text markieren.
+
+            3. Mit der Taste &apos;Speichern&apos; in einer Datei speichern.</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/locale_ga.ts
@@ -0,0 +1,177 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ga_IE">
+<context>
+    <name>QPlatformTheme</name>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="251"/>
+        <source>Open</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="264"/>
+        <source>Import</source>
+        <translation>Iompórtáil</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="311"/>
+        <source>Cancel</source>
+        <translation>Cealaigh</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="324"/>
+        <source>Export</source>
+        <translation>Easpórtáil</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="366"/>
+        <source>Save</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>abc_ImpEx</name>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="33"/>
+        <source>This plugin imports ABC notation from a file or the clipboard and exports the current score to ABC.
+Executables of abc2xml and/or xml2abc are required.</source>
+        <translation>Iompórtálann an breiseán seo nodaireacht ABC as comhad nó as an ghearrthaisce agus easpórtálann sé an scór faoi láthair mar ABC.\nIs gá clar abc2xml agus/nó xml2abc chun é a dheanamh.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="49"/>
+        <source>
+ INSTALLATION
+ ==========
+
+        Copy abc2xml.py and/or xml2abc.py into this plugin&apos;s folder:
+        %1
+
+        Should you already have the files in a different location,
+        enter their full path into the ini file in the same folder.
+
+
+        If you are running Windows, make sure that Python is installed
+        and that the path to Python is in your PATH environment variable.
+
+        Alternatively, use abc2xml.exe and xm2abc.exe files instead.</source>
+        <translation>
+ SUITEÁIL
+ ======
+
+        Cóipeáil abc2xml.py agus/nó xml2abc.py san fhillteán den bhreisán seo:
+        %1
+
+        Má tá na comhaid in áit difriúil agat fós, cuir a gcosán
+        iomlán isteach sa chomhad ini san fhillteán céanna.
+
+
+        Má tá Windows ar siúl agat, bí cinnte go bhfuil Python suiteáilte agat
+        agus an cosán go Python san athróg timpeallachta PATH.
+
+        De rogha air sin, bain usáid as abc2xml.exe agus xm2abc.exe.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="105"/>
+        <location filename="../abc_ImpEx.qml" line="303"/>
+        <location filename="../abc_ImpEx.qml" line="352"/>
+        <source>Python not found!.
+Please check your installation and
+environment variables.</source>
+        <translation>Níl Python a fháil!
+Seiceáil do shuiteáil, le do thoil,
+agus na hathróga timpeallachta.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="148"/>
+        <source>Please choose a file</source>
+        <translation>Roghnaigh comhad le do thoil</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="162"/>
+        <source>Please select a destination</source>
+        <translation>Roghnaigh ceann scríbe le do thoil</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="197"/>
+        <source>This plugin needs abc2xml and/or xml2abc from &lt;a href=&apos;https://wim.vree.org/svgParse&apos;&gt;wim.vree.org&lt;/a&gt; to work.</source>
+        <translation>Is gá abc2xml agus/nó xml2abc de &lt;a href=&apos;https://wim.vree.org/svgParse&apos;&gt;wim.vree.org&lt;/a&gt;  leis an breisán seo.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="240"/>
+        <source>score line-break = $</source>
+        <translation>line nua scóir = $</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="295"/>
+        <source>No abc to import.</source>
+        <translation>Níl aon abc le hiompórtáil.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="301"/>
+        <source>This function needs abc2xml.
+Copy file into abc_ImpEx folder
+or enter path into abc_ImpEx.ini.</source>
+        <translation>Is gá ab2xml leis an bhfeidhm seo.
+Cóipeáil an comhad san fhillteán abc_ImpEx
+nó cuir isteach a chosán san abc_ImpEx.ini.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="350"/>
+        <source>This function needs xml2abc.
+Copy file into abc_ImpEx folder
+or enter path into abc_ImpEx.ini.</source>
+        <translation>Is gá xml2abc leis an bhfeidhm seo.
+Cóipeáil an comhad san fhillteán abc_ImpEx
+nó cuir isteach a chosán san abc_ImpEx.ini.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="357"/>
+        <source>No score to export.</source>
+        <translation>Níl aon scór le heaspórtáil.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="378"/>
+        <source>No abc to save.</source>
+        <translation>Níl aon abc le sábháil.</translation>
+    </message>
+    <message>
+        <location filename="../abc_ImpEx.qml" line="58"/>
+        <source>
+ IMPORT
+ ======
+
+            1. Paste ABC or select file using button &apos;Open&apos;.
+
+            2. Change ABC as desired and/or select part of it.
+
+            3. Import into score using button &apos;Import&apos;.
+            
+ EXPORT
+ ======
+
+            1. To translate active score, click button &apos;Export&apos;.
+
+            2. Change ABC as desired and/or select part of it.
+
+            3. Save into file using button &apos;Save&apos;.</source>
+        <translation>
+ IOMPÓRTÁIL
+ ==========
+
+            1. Greamhaigh ABC nó roghnaigh comhad leis an cnaipe &apos;Oscail&apos;.
+
+            2. Athraigh ABC de réir mar atá ag teastáil agus/nó roghnaigh cuid den téacs.
+
+            3. Iomportáil mar scór ag baint usáid as cnaipe &apos;Iomportáil&apos;.
+            
+ EASPÓRTÁIL
+ ==========
+
+            1. Chun an scór mar atá a aistriú, cliceáil cnaipe &apos;Easpórtáil&apos;.
+
+            2. Athraigh ABC de réir mar atá ag teastail agus/nó roghnaigh cuid den téacs.
+
+            3. Cliceáil ar &apos;Sábháil&apos; chun an ABC a shábháil i gcomhad.</translation>
+    </message>
+</context>
+</TS>
